a70e097a93c352a73668c73bbeecd64432503e41
Siddharth Shankar <sdshank2@yahoo.com>
2025-04-06 14:38:16 -0400

    Create NotFoundPage.jsx

---
+++ src/Pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+    return(
+        <div className="text-black dark:text-white h-[864px]">
+            <h1 className="text-center">Page Not Found.</h1>
+        </div>
+    )
+}
